3c2915e00e8cd06a664264eb3d5e34f9902d8380
zhaochuanzhen <zhaochuanzhen@hxrainbow.com>
2019-12-26 18:21:00 +0800

    函数

---
+++ practice02_function/test01.py
@@ -0,0 +1,25 @@
+"""
+desc: 函数
+author: 赵传真
+date: 2019年12月26日17:03:16
+"""
+
+# !/usr/bin/env python3
+# -*- encoding: UTF-8 -*-
+
+print(abs(-12.34))
+
+print(max(1, 2, 44, 5))
+
+print(int('123'))
+print(float('123.12'))
+print(int(float('123.12')))
+print(bool(55))
+
+a = abs
+print(a(-22))
+
+n1 = 255
+n2 = 1000
+print(hex(n1))
+print(hex(n2))
