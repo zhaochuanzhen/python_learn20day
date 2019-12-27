0431fe581ea5bcde55116cb659b731d11b4debf0
zhaochuanzhen <zhaochuanzhen@hxrainbow.com>
2019-12-27 17:58:45 +0800

    斐波拉契数列

--- practice03_advance_feature/test04.py
+++ practice03_advance_feature/test04.py
@@ -7,4 +7,21 @@ date: 2019年12月27日16:26:40
 # !/usr/bin/env python3
 # -*- encoding: UTF-8 -*-
 
+g1 = (x ** 2 for x in range(1, 11))
+for n in g1:
+    print(n)
 
+print('=============斐波拉契数列=================')
+
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+
+for g in fib(7):
+    print(g)
