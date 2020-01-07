8a80dcc7c35c7dd08a63a78d9d2e689c8be95bcf
zhaochuanzhen <zhaochuanzhen@hxrainbow.com>
2020-01-07 20:19:12 +0800

    模块

--- practice04_function_programe/test09.py
+++ practice04_function_programe/test09.py
@@ -3,6 +3,12 @@ desc: 偏函数
 author: 赵传真
 date: 2020年1月6日15:25:53
 """
+import functools
+
 print(int('123456'))
 print(int('123456', base=8))
 print(int('11001010101', 2))
+
+int2 = functools.partial(int, base=2)
+
+print(int2('1111'))
