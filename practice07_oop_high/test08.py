bcbb8b9317415fd4c585fac750efe1f29836e53f
tripperGo <ai_taurus@163.com>
2020-01-10 23:03:29 +0800

    metaclass

--- practice07_oop_high/test08.py
+++ practice07_oop_high/test08.py
@@ -1,3 +1,24 @@
-"""元类"""
+"""元类学习"""
 __author__ = '赵传真'
 
+from myHello import Hello
+
+h = Hello()
+h.hello('赵传真')
+print(type(h))
+print(type(Hello))
+
+
+class ListMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        attrs['add'] = lambda self, value: self.append(value)
+        return type.__new__(cls, name, bases, attrs)
+
+
+class MyList(list, metaclass=ListMetaclass):
+    pass
+
+
+l = MyList()
+l.add(1)
+print(l)
